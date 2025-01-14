b7f38b1c9303ac11ed6837aa7f7e549c59e49b36
Harkant <harkantmagotra@gmail.com>
2025-01-14 23:43:06 +0530

    insert Recursion once check it is not working

--- src/com/harkant/LL.java
+++ src/com/harkant/LL.java
@@ -72,6 +72,22 @@ public class LL {
         return val;
     }
 
+    // insert using recursion
+
+    public void insertRec(int val, int index) {
+        head = insertRec(val, index, head);
+    }
+
+    private Node insertRec(int val, int index, Node node) {
+        if (index == 0) {
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+        node.next = insertRec(val, index--, node.next);
+        return node;
+    }
+
     public void insert(int val, int index) {
         if (index == 0) {
             insertFirst(val);
--- src/com/harkant/Main.java
+++ src/com/harkant/Main.java
@@ -2,20 +2,22 @@ package com.harkant;
 
 public class Main {
     public static void main(String[] args) {
-        // LL list = new LL();
-        // list.insertFirst(34);
-        // list.insertFirst(43);
-        // list.insertFirst(4);
-        // list.insertFirst(3);
-        // list.insertLast(10);
-        // list.insert(200, 1);
-        // list.display();
-        // System.out.println(list.deleteFirst());
-        // list.display();
-        // System.out.println(list.deleteLast());
-        // list.display();
-        // System.out.println(list.delete(2));
-        // list.display();
+        LL list = new LL();
+        list.insertFirst(34);
+        list.insertFirst(43);
+        list.insertFirst(4);
+        list.insertFirst(3);
+        list.insertLast(10);
+        list.insert(200, 1);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(2));
+        list.display();
+        list.insertRec(213, 2);
+        list.display();
 
         // DLL list = new DLL();
         // list.insertFirst(1);
@@ -28,14 +30,14 @@ public class Main {
         // list.insertAfterVal(5, 6);
         // list.display();
 
-        CircularLL list = new CircularLL();
+        // CircularLL list = new CircularLL();
 
-        list.insert(1);
-        list.insert(2);
-        list.insert(3);
-        list.insert(4);
-        list.display();
-        list.delete(3);
-        list.display();
+        // list.insert(1);
+        // list.insert(2);
+        // list.insert(3);
+        // list.insert(4);
+        // list.display();
+        // list.delete(3);
+        // list.display();
     }
 }
